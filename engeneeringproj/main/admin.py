1c0db6b464fa12d8528d9f390630a960fc949b4a
Irina <parakerta@mail.ru>
2020-12-29 17:09:43 +0300

    add import and export for news

--- engeneeringproj/main/admin.py
+++ engeneeringproj/main/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 from .models import News, Jobs, Documents, Blog, Articles, Questions, Answers
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources, fields
+from import_export.widgets import ForeignKeyWidget
 
 
-admin.site.register(News)
+class NewsResource(resources.ModelResource):
+    class Meta:
+        model = News
+
+
+class NewsAdmin(ImportExportModelAdmin):
+    resource_class = NewsResource
+
+
+admin.site.register(News, NewsAdmin)
 admin.site.register(Jobs)
 admin.site.register(Documents)
 admin.site.register(Blog)
