70ea69988d621a420545d593324f0e77d8b98749
Irina <parakerta@mail.ru>
2021-06-14 17:22:56 +0300

    search

--- engeneeringproj/ad/urls.py
+++ engeneeringproj/ad/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('', views.index, name='ads_index'),
-    path('<id_ad>', views.detail, name='ad_detail')
+    path('<id_ad>', views.detail, name='ad_detail'),
+    path('search/', views.search, name='search_results'),
 ]
--- engeneeringproj/ad/views.py
+++ engeneeringproj/ad/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from .models import Ad, Credit
-
+from django.db.models import Q
 
 def index(request):
     ads = Ad.objects.all()
     count = Ad.objects.count()
     return render(request, 'ad/index.html', {'ads': ads, 'count': count})
 
+def search(request):
+    query = request.GET.get('q')
+    object_list = Ad.objects.filter(
+        Q(title__icontains=query) | Q(geo__icontains=query)
+    )
+    count =  object_list.count()
+    return render(request, 'ad/search_results.html', {'object_list': object_list, 'query': query, 'count': count })
 
 def detail(request, id_ad):
     ad = Ad.objects.get(id_ad=id_ad)
