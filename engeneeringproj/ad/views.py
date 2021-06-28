03b09194690823c94c483231c7a6bc9b50059887
Irina <parakerta@mail.ru>
2021-06-28 16:09:46 +0300

    crud and roles

--- engeneeringproj/ad/views.py
+++ engeneeringproj/ad/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Ad
 from django.db.models import Q
 from .forms import AdForm
+from django.http import HttpResponseRedirect
 
 def index(request):
     ads = Ad.objects.all()
@@ -31,6 +32,22 @@ def search(request):
     count =  object_list.count()
     return render(request, 'ad/search_results.html', {'object_list': object_list, 'query': query, 'count': count })
 
+
 def detail(request, id_ad):
+    context ={}
+
     ad = Ad.objects.get(id_ad=id_ad)
-    return render(request, 'ad/detail.html', {'ad': ad })
+    form = AdForm(request.POST or None)
+
+    if form.is_valid():
+        ad_update = form.save(commit=False)
+        ad_update.author = request.user
+        ad_update.save()
+  
+    if request.method =="POST":
+        ad.delete()
+        return HttpResponseRedirect("/ads")
+
+    context['form']= form    
+    context['ad']= ad
+    return render(request, 'ad/detail.html', context)
