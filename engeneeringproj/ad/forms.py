f96086f6662c8ea335abf0522c0ebb76f0c1372d
Irina <parakerta@mail.ru>
2021-06-29 12:38:25 +0300

    validation

--- engeneeringproj/ad/forms.py
+++ engeneeringproj/ad/forms.py
@@ -5,7 +5,7 @@ from .models import Ad
 class AdForm(forms.ModelForm):
     title = forms.CharField(label='Название', required=True, widget=forms.TextInput(attrs={"class":"form-control"}))
     description = forms.CharField(label='Описание', widget=forms.Textarea(attrs={"class":"form-control"}))
-    price = forms.IntegerField(label='Стоимость', widget=forms.TextInput(attrs={"class":"form-control"}))
+    price = forms.CharField(label='Стоимость', widget=forms.TextInput(attrs={"class":"form-control", 'type':'number'}))
     geo = forms.CharField(label='Местоположение', widget=forms.TextInput(attrs={"class":"form-control"}))
     class Meta:
         model = Ad
